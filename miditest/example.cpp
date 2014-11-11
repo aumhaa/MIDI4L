e5c6dd775795825c06de265bf1322114fa9467dc
Adam Murray <adamjmurray@gmail.com>
2014-11-11 14:53:25 -0800

    initial support for dumping the list of ports (input ports only so far)

--- miditest/example.cpp
+++ miditest/example.cpp
@@ -9,9 +9,25 @@ class Example : public MaxCpp6<Example> {
     
 public:
 	Example(t_symbol * sym, long ac, t_atom * av) { 
-		setupIO(2, 2); // inlets / outlets
+		setupIO(2, 3); // inlets / outlets
         
-        initRtMidi();
+        try {
+            midiin = new RtMidiIn();
+        }
+        catch ( RtMidiError &error ) {
+            error("RtMidiIn constructor failure");
+            error.printMessage(); // TODO: print this to the Max console
+        }
+        
+        try {
+            midiout = new RtMidiOut();
+        }
+        catch ( RtMidiError &error ) {
+            error("RtMidiOut consturctor failure");
+            error.printMessage(); // TODO: print this to the Max console
+        }
+        
+        refreshPorts();
         
         // TODO: maybe only do the following print-outs if a verbose attr is set, or a DEBUG flag is enabled during dev
         printPorts();
@@ -23,101 +39,100 @@ public:
     }
 	
     
-	// methods:
-	void bang(long inlet) { 
-		outlet_bang(m_outlets[0]);
+	void bang(long inlet) {
+        t_symbol *appendSym = gensym("append"); // make a constant? and for clear?
+        char cPortName[MAX_PORT_NAME_SIZE];
+        void *dumpOutlet = m_outlets[1]; // TODO: make a constant for this
+        t_atom portNameAtoms[1];
+        
+        outlet_anything(dumpOutlet, gensym("clear"), 0, NULL);
+        
+        // TODO: need to switch between inPortMap or outPortMap depending on whether we're in input or output mode
+        std::map<std::string,int> portMap = inPortMap;
+        
+        for( std::map<std::string,int>::iterator iter=portMap.begin(); iter!=portMap.end(); iter++ ) {
+            std::string portName = (*iter).first;
+            strncpy(cPortName, portName.c_str(), MAX_PORT_NAME_SIZE);
+            cPortName[MAX_PORT_NAME_SIZE - 1] = NULL;
+
+            atom_setsym(&portNameAtoms[0], gensym(cPortName));
+            
+            outlet_anything(dumpOutlet, appendSym, 1, portNameAtoms);
+        }
 	}
     
     
-	void testfloat(long inlet, double v) { 
-		outlet_float(m_outlets[0], v);
-	}
-    
-    
-	void testint(long inlet, long v) { 
-		outlet_int(m_outlets[0], v);
-	}
-    
-    
-	void test(long inlet, t_symbol * s, long ac, t_atom * av) { 
-		outlet_anything(m_outlets[1], gensym("test"), ac, av); 
-	}
+    // Refresh the port list when a MIDI device is plugged in or unplugged.
+    // TODO: need to test what happens when we've already opened a port for a device that is then unplugged
+    void refresh(long inlet, t_symbol * s, long ac, t_atom * av) {
+        refreshPorts();
+    }
     
     
 private:
-    RtMidiIn*  midiin  = NULL;
-    RtMidiOut* midiout = NULL;
+    RtMidiIn  *midiin  = NULL;
+    RtMidiOut *midiout = NULL;
     int numInPorts  = -1;
     int numOutPorts = -1;
     std::map<std::string, int> inPortMap;
     std::map<std::string, int> outPortMap;
     
     
-    void initRtMidi() {
+    // Get the list of available ports. Can be called repeatedly to regenerate the list if a MIDI device is plugged in or unplugged.
+    void refreshPorts() {
         std::string portName;
-        
-        try {
-            midiin = new RtMidiIn();
-        }
-        catch ( RtMidiError &error ) {
-            error("RtMidiIn constructor failure");
-            error.printMessage(); // TODO: print this to the Max console
-            return;
-        }
-        
-        try {
-            midiout = new RtMidiOut();
-        }
-        catch ( RtMidiError &error ) {
-            error("RtMidiOut consturctor failure");
-            error.printMessage(); // TODO: print this to the Max console
-            return;
-        }
-        
-        numInPorts = midiin->getPortCount();
-        for ( int i=0; i<numInPorts; i++ ) {
-            try {
-                portName = midiin->getPortName(i);
-                inPortMap[portName] = i;
+        inPortMap.clear();
+        outPortMap.clear();
+
+        if(midiin) {
+            numInPorts = midiin->getPortCount();
+            for ( int i=0; i<numInPorts; i++ ) {
+                try {
+                    portName = midiin->getPortName(i);
+                    inPortMap[portName] = i;
+                }
+                catch ( RtMidiError &error ) {
+                    error("Error getting MIDI input port name");
+                    error.printMessage(); // TODO: print this to the Max console
+                }
             }
-            catch ( RtMidiError &error ) {
-                error("Error getting MIDI input port name");
-                error.printMessage(); // TODO: print this to the Max console
-            }
-            
         }
         
-        numOutPorts = midiout->getPortCount();
-        for ( int i=0; i<numOutPorts; i++ ) {
-            try {
-                portName = midiout->getPortName(i);
-                outPortMap[portName] = i;
-            }
-            catch (RtMidiError &error) {
-                error("Error getting MIDI output port name");
-                error.printMessage(); // TODO: print this to the Max console
+        if(midiout) {
+            numOutPorts = midiout->getPortCount();
+            for ( int i=0; i<numOutPorts; i++ ) {
+                try {
+                    portName = midiout->getPortName(i);
+                    outPortMap[portName] = i;
+                }
+                catch (RtMidiError &error) {
+                    error("Error getting MIDI output port name");
+                    error.printMessage(); // TODO: print this to the Max console
+                }
             }
         }
     }
+
     
     
+    // Print the port names to the Max console
     void printPorts() {
-        std::string portName;
         char cPortName[MAX_PORT_NAME_SIZE];
         
         post("MIDI input Port count: %u", numInPorts);
+        
         for( std::map<std::string,int>::iterator iter=inPortMap.begin(); iter!=inPortMap.end(); iter++ ) {
-            portName = (*iter).first;
+            std::string portName = (*iter).first;
             strncpy(cPortName, portName.c_str(), MAX_PORT_NAME_SIZE);
             cPortName[MAX_PORT_NAME_SIZE - 1] = NULL;
             post("input %u: %s", (*iter).second, cPortName);
         }
         
         post(" ");
-        
         post("MIDI output port count: %u", numOutPorts);
+        
         for( std::map<std::string,int>::iterator iter=outPortMap.begin(); iter!=outPortMap.end(); iter++ ) {
-            portName = (*iter).first;
+            std::string portName = (*iter).first;
             strncpy(cPortName, portName.c_str(), MAX_PORT_NAME_SIZE);
             cPortName[MAX_PORT_NAME_SIZE - 1] = NULL;
             post("output %u: %s", (*iter).second, cPortName);
@@ -129,7 +144,5 @@ private:
 C74_EXPORT int main(void) {
 	Example::makeMaxClass("example");
 	REGISTER_METHOD(Example, bang);
-	REGISTER_METHOD_FLOAT(Example, testfloat);
-	REGISTER_METHOD_LONG(Example, testint);
-	REGISTER_METHOD_GIMME(Example, test);
+    REGISTER_METHOD_GIMME(Example, refresh);
 }
