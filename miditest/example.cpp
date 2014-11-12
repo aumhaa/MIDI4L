d3bd83f0caae1dce5372c8b9748e5940e736683f
Adam Murray <adamjmurray@gmail.com>
2014-11-11 16:39:36 -0800

    can now find the RtMidi port index for a given "inport" or "outport"

--- miditest/example.cpp
+++ miditest/example.cpp
@@ -32,6 +32,7 @@ public:
         }
         
         refreshPorts();
+        printPorts(); // TODO: comment this out before shipping
 	}
 	
     ~Example() {
@@ -42,7 +43,7 @@ public:
     
     void assist(void *b, long io, long index, char *msg) {
         if (io == ASSIST_INLET) {
-            strncpy_zero(msg, "send MIDI output (list), list ports (bang), set port (port name)", MAX_STR_SIZE);
+            strncpy_zero(msg, "send MIDI output (list), list ports (bang), set input port (inport name), set output port (outport name)", MAX_STR_SIZE);
         }
         else if (io==ASSIST_OUTLET) {
             switch (index) {
@@ -67,19 +68,50 @@ public:
         dumpPorts(m_outlets[OUTLET_OUTPORTS], outPortMap);
     }
     
+    void inport(long inlet, t_symbol *s, long ac, t_atom *av) {
+        t_symbol *portName = _sym_nothing;
+        
+        if( atom_arg_getsym(&portName, 0, ac, av) == MAX_ERR_NONE ) {
+            post("Got inport name %s", *portName);
+            int portIndex = getPortIndex(inPortMap, portName);
+            if (portIndex >= 0) {
+                post("Found port at index %i", portIndex);
+            }
+        }
+        else {
+            error("Invalid inport message. A portname is required");
+        }
+    }
+    
+    void outport(long inlet, t_symbol *s, long ac, t_atom *av) {
+        t_symbol *portName = _sym_nothing;
+        
+        if( atom_arg_getsym(&portName, 0, ac, av) == MAX_ERR_NONE ) {
+            post("Got outport name %s", *portName);
+            int portIndex = getPortIndex(outPortMap, portName);
+            if (portIndex >= 0) {
+                post("Found port at index %i", portIndex);
+            }
+        }
+        else {
+            error("Invalid outport message. A portname is required");
+        }
+    }
+    
     
 private:
     RtMidiIn  *midiin  = NULL;
     RtMidiOut *midiout = NULL;
     int numInPorts  = -1;
     int numOutPorts = -1;
-    std::map<std::string, int> inPortMap;
-    std::map<std::string, int> outPortMap;
+    std::map<t_symbol *, int> inPortMap;
+    std::map<t_symbol *, int> outPortMap;
     
     
     // Get the list of available ports. Can be called repeatedly to regenerate the list if a MIDI device is plugged in or unplugged.
     void refreshPorts() {
-        std::string portName;
+        char cPortName[MAX_STR_SIZE];
+        
         inPortMap.clear();
         outPortMap.clear();
 
@@ -87,8 +119,11 @@ private:
             numInPorts = midiin->getPortCount();
             for ( int i=0; i<numInPorts; i++ ) {
                 try {
-                    portName = midiin->getPortName(i);
-                    inPortMap[portName] = i;
+                    std::string portName = midiin->getPortName(i);
+                    strncpy(cPortName, portName.c_str(), MAX_STR_SIZE);
+                    cPortName[MAX_STR_SIZE - 1] = NULL;
+                    
+                    inPortMap[gensym(cPortName)] = i;
                 }
                 catch ( RtMidiError &error ) {
                     error("Error getting MIDI input port name");
@@ -101,8 +136,12 @@ private:
             numOutPorts = midiout->getPortCount();
             for ( int i=0; i<numOutPorts; i++ ) {
                 try {
-                    portName = midiout->getPortName(i);
-                    outPortMap[portName] = i;
+                    std::string portName = midiout->getPortName(i);
+                    strncpy(cPortName, portName.c_str(), MAX_STR_SIZE);
+                    cPortName[MAX_STR_SIZE - 1] = NULL;
+                    
+                    // TODO: not sure if we should store the pointers or what they reference?
+                    outPortMap[gensym(cPortName)] = i;
                 }
                 catch (RtMidiError &error) {
                     error("Error getting MIDI output port name");
@@ -110,50 +149,51 @@ private:
                 }
             }
         }
-        // printPorts();
     }
 
+    
+    // Returns the port index for the given portName in the given portMap, or -1 if the port is not found
+    int getPortIndex(std::map<t_symbol*,int> portMap, t_symbol *portName) {
+        std::map<t_symbol*,int>::iterator iter = portMap.find(portName);
+        if (iter == portMap.end()) {
+            error("Port not found: %s", *portName);
+            return -1;
+        }
+        else {
+            return iter->second;
+        }
+    }
     
     
     // Print the port names to the Max console
     void printPorts() {
-        char cPortName[MAX_STR_SIZE];
-        
         post("MIDI input Port count: %u", numInPorts);
         
-        for( std::map<std::string,int>::iterator iter=inPortMap.begin(); iter!=inPortMap.end(); iter++ ) {
-            std::string portName = (*iter).first;
-            strncpy(cPortName, portName.c_str(), MAX_STR_SIZE);
-            cPortName[MAX_STR_SIZE - 1] = NULL;
-            post("input %u: %s", (*iter).second, cPortName);
+        for( std::map<t_symbol*,int>::iterator iter=inPortMap.begin(); iter!=inPortMap.end(); iter++ ) {
+            t_symbol *portName = (*iter).first;
+            post("input %u: %s", (*iter).second, *portName);
         }
         
         post(" ");
         post("MIDI output port count: %u", numOutPorts);
         
-        for( std::map<std::string,int>::iterator iter=outPortMap.begin(); iter!=outPortMap.end(); iter++ ) {
-            std::string portName = (*iter).first;
-            strncpy(cPortName, portName.c_str(), MAX_STR_SIZE);
-            cPortName[MAX_STR_SIZE - 1] = NULL;
-            post("output %u: %s", (*iter).second, cPortName);
+        for( std::map<t_symbol*,int>::iterator iter=outPortMap.begin(); iter!=outPortMap.end(); iter++ ) {
+            t_symbol* portName = (*iter).first;
+            post("output %u: %s", (*iter).second, *portName);
         }
     }
     
     
-    void dumpPorts(void *outlet, std::map<std::string,int> portMap) {
+    void dumpPorts(void *outlet, std::map<t_symbol*,int> portMap) {
         t_symbol *appendSym = gensym("append"); // make a constant? and for clear?
-        char cPortName[MAX_STR_SIZE];
-        t_atom portNameAtoms[1];
+        t_atom atoms[1];
         
         outlet_anything(outlet, gensym("clear"), 0, NULL);
         
-        for( std::map<std::string,int>::iterator iter=portMap.begin(); iter!=portMap.end(); iter++ ) {
-            std::string portName = (*iter).first;
-            strncpy(cPortName, portName.c_str(), MAX_STR_SIZE);
-            cPortName[MAX_STR_SIZE - 1] = NULL;
-            atom_setsym(&portNameAtoms[0], gensym(cPortName));
-            
-            outlet_anything(outlet, appendSym, 1, portNameAtoms);
+        for( std::map<t_symbol*,int>::iterator iter=portMap.begin(); iter!=portMap.end(); iter++ ) {
+            t_symbol *portName = (*iter).first;
+            atom_setsym(&atoms[0], portName);
+            outlet_anything(outlet, appendSym, 1, atoms);
         }
     }
 };
@@ -161,7 +201,8 @@ private:
 
 C74_EXPORT int main(void) {
 	Example::makeMaxClass("example");
-	REGISTER_METHOD(Example, bang);
-    
     REGISTER_METHOD_ASSIST(Example, assist);
+    REGISTER_METHOD(Example, bang);
+    REGISTER_METHOD_GIMME(Example, inport);
+    REGISTER_METHOD_GIMME(Example, outport);
 }
